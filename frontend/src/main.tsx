c633749364612c1b9b357357f8651cc13ff1b895
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2025-05-11 12:26:43 -0300

    feat: add sonner for toast notifications and update HomePage component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 import "./index.css";
 import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { toast } from "sonner";
+
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of the application.</p>
+      <button onClick={() => toast("My first toast")}>Give me a toast</button>
     </div>
   );
 };
