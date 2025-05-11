23f2c20e0029d42aed1b4db799e7f0425c7bd8fb
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2025-05-11 18:40:24 -0300

    feat: update NotFound page with new styles and navigation buttons

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,10 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-600">404 Not Found</h1>
-      <p className="mt-4 text-lg text-gray-700">
-        The page you are looking for does not exist.
-      </p>
+    <div className="h-svh">
+      <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-[7rem] leading-tight font-bold">404</h1>
+        <span className="font-medium">Ops! Página não encontrada!</span>
+        <p className="text-muted-foreground text-center">
+          Parece que a página que você está procurando <br />
+          não existe ou pode ter sido removida.
+        </p>
+        <div className="mt-6 flex gap-4">
+          <Button variant="outline" onClick={() => navigate(-1)}>
+            Voltar
+          </Button>
+          <Button onClick={() => navigate("/")}>
+            Ir para a Página Inicial
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
