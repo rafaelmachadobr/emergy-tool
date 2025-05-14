b78bd9a9cfcac4e99caf31c91cfd2e699b76cc56
Samara Marques <samarassk@gmail.com>
2025-05-13 23:28:28 -0300

    fix: url with swagger conf

--- backend/src/emergy_app/urls.py
+++ backend/src/emergy_app/urls.py
@@ -1,26 +1,25 @@
-"""
-URL configuration for emergy_app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Configuração do Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API de Usuários",
+        default_version='v1',
+        description="Documentação da API de Usuários com Swagger",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="samarassk@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
 )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +27,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
     path('api/lci/', include('lci.urls')),
     path('api/', include('accounts.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
 ]
