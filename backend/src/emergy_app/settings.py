5854324386672ab8e28c8bbca20f2e3d68e0a442
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2025-05-24 00:01:22 -0300

    fix: allow access from all origins by setting ALLOWED_HOSTS

--- backend/src/emergy_app/settings.py
+++ backend/src/emergy_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
