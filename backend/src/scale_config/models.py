808e4ed5fd859ca68fc6afea6c4dbb82680e8bb5
Samara Marques <samarassk@gmail.com>
2025-05-25 18:11:17 -0300

    feat: emergy calculation models

--- backend/src/scale_config/models.py
+++ backend/src/scale_config/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from lci.models import LCIMatrix
+
 class ScaleConfig(models.Model):
     name = models.CharField(max_length=100)
     config = models.JSONField()
@@ -8,3 +10,13 @@ class ScaleConfig(models.Model):
 
     class Meta:
         db_table = 'scale_config'
+
+class EmergyCalculation(models.Model):
+    matrix = models.ForeignKey("lci.LCIMatrix", on_delete=models.CASCADE)
+    scale_config = models.ForeignKey("scale_config.ScaleConfig", on_delete=models.CASCADE)
+    useful_product = models.FloatField()
+    results = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"EmergyCalculation #{self.id} - Matrix {self.matrix.id}"
