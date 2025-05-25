f16fb2ebbc2c0f17f327d60d3fa68a42f425fce3
Samara Marques <samarassk@gmail.com>
2025-05-24 21:40:53 -0300

    feat: lci model

--- backend/src/lci/models.py
+++ backend/src/lci/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class LCIMatrix(models.Model):
+    name = models.CharField(max_length=100)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class LCICell(models.Model):
+    matrix = models.ForeignKey(LCIMatrix, on_delete=models.CASCADE, related_name='cells')
+    row = models.CharField(max_length=100)
+    column = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
