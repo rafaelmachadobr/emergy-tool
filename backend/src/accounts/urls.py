ece5f229edba9a5d33b104ffcbef8230d98ed35b
Samara Marques <samarassk@gmail.com>
2025-05-25 19:43:31 -0300

    feat: user info

--- backend/src/accounts/urls.py
+++ backend/src/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from accounts.views import UserViewSet
+from accounts.views import UserViewSet, UserInfoView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('user-info/', UserInfoView.as_view(), name='user-info'),
 ]
